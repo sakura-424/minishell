e21cbe507ee0d2392da67cbd05c244a6abb0f49f
skatsuya <skatsuya@c3r5s6.42tokyo.jp>
2026-01-13 20:27:12 +0900

    signal.hが読み込まれない不具合を修正

--- include/sig.h
+++ include/sig.h
@@ -6,17 +6,17 @@
 /*   By: skatsuya < skatsuya@student.42tokyo.jp>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 03:13:27 by sakurako          #+#    #+#             */
-/*   Updated: 2026/01/13 20:01:25 by skatsuya         ###   ########.fr       */
+/*   Updated: 2026/01/13 20:26:23 by skatsuya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIG_H
 # define SIG_H
 
+# include <signal.h>
 # include "minishell.h"
 # include <readline/history.h>
 # include <readline/readline.h>
-# include <signal.h>
 # include <sys/wait.h>
 
 extern volatile sig_atomic_t	g_sig;
